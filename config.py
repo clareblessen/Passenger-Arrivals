34f887dbcfedf9e56e37db35ad4b76852dd316ae
Clare Blessen <54646785+clareblessen@users.noreply.github.com>
2020-01-17 08:27:34 -0500

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-user = 'admin'
-host = 'flatiron.cemfxpy3sbon.us-east-2.rds.amazonaws.com'
-password = 'Tennis01!'
-api_key = '8RjJcohpiGfnPS1kR9Qsq9iFp1IdCRrj'
-goodreadsemail = 'clarekblessen@gmail.com'
